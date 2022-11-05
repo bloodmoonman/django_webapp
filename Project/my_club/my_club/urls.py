91749f4ef1ba713602b0dfa5636abc60fef325e0
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-05 18:15:02 +0300

    Update urls.py

--- Project/my_club/my_club/urls.py
+++ Project/my_club/my_club/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
-    path('members/', include('members.urls')), #AS WE DID FOR THE FIRST APP, EVENTS, WE WANT THIS ORIGINAL URLS.PY TO POINT TO MEMBERS APP TOO.
-    path('members/', include('django.contrib.auth.urls')), #THIS IS FOR, REFERENCING THE AUTHENTICATION, THIS WILL ALLOW US TO USE BUNCH OF URLS THAT COMES WITH THE AUTHENTICATION    
+    path('members/', include('members.urls')),
+    path('members/', include('django.contrib.auth.urls')),
 ]
