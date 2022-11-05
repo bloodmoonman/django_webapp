15457a071e848a1dc469db8aa11a32aa0e7ffcbc
Aykan Seper <96862833+bloodmoonman@users.noreply.github.com>
2022-11-05 18:07:39 +0300

    Update forms.py

--- Project/my_club/events/forms.py
+++ Project/my_club/events/forms.py
@@ -7,7 +7,7 @@ from .models import Venue, Event
 
 class VenueForm(ModelForm):
     class Meta:
-        model = Venue            #MODEL IS THE VENUE 
+        model = Venue            
         fields = ('name', 'address', 'zip_code', 'phone', 'web', 'email_address',)  #THESE ARE THE FIELDS WE WANT USERS TO SEE ON THE WEB PAGE. WE COULD USE '__all__' FOR ALL FIELDS.
         widgets = {
         'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue Name'}),
